64cce212ae5ff6e608931341a1c0fdfd63d22336
kutaykalkan <kutaykalkan@hotmail.com>
2018-05-09 14:12:51 -0400

    Initial commit

---
+++ StackImplementation.Tests/StackImplementationTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StackImplementation.Tests
+{
+    [TestFixture]
+    public class StackImplementationTests
+    {
+        private Stack<int> _stack;
+        [Test]
+        public void TestPushPop()
+        {
+            _stack = new Stack<int>();
+            _stack.Push(1);
+            Assert.AreEqual(1, _stack.Pop());
+        }
+
+        [Test]
+        public void TestEnsureCapacity()
+        {
+            _stack = new Stack<int>(1);
+            _stack.Push(1);
+            _stack.Push(2);
+            Assert.AreEqual(2, _stack.Capacity);
+            Assert.AreEqual(2, _stack.Size);
+        }
+
+        [Test]
+        public void TestIsEmpty()
+        {
+            _stack = new Stack<int>(1);
+            Assert.AreEqual(true, _stack.IsEmpty);
+        }
+
+        [Test]
+        public void WhenStackIsEmptyThrowException()
+        {
+            _stack = new Stack<int>();
+            Assert.Throws<Exception>(() => _stack.Pop(), "There are no items in the stack!");
+        }
+
+        [Test]
+        public void WhenCapacityIsBelowOrEqualToZeroThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => _stack = new Stack<int>(-1), "Capacity can not be 0");
+        }
+    }
+}
---
+++ StackImplementation/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace StackImplementation
+{
+    /// <summary>
+    /// Simple stack implementation with Array. This version does not reduce capacity as items get deleted.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack<T>
+    {
+        private T[] _stack;
+
+        public Stack(int capacity = 8)
+        {
+            if(capacity <= 0)
+                throw new ArgumentException("Capacity can not be 0");
+            Capacity = capacity;
+            _stack = new T[capacity];
+        }
+
+        public int Size { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public bool IsEmpty => Size == 0;
+
+        public void Push(T item)
+        {
+            EnsureCapacity();
+            _stack[Size++] = item;
+        }
+
+        public T Pop()
+        {
+            if(Size <= 0)
+                throw new Exception("There are no items in the stack!");
+            T returnVal = _stack[Size-1];
+            _stack[Size-1] = default(T);
+            Size--;
+
+            return returnVal;
+        }
+
+        private void EnsureCapacity()
+        {
+            if (Size == Capacity)
+            {
+                var tmp = _stack;
+                Capacity = Capacity * 2;
+                _stack = new T[Capacity];
+                Array.Copy(tmp, _stack, tmp.Length);
+            }
+        }
+    }
+}
